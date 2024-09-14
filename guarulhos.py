07095224f372d4af1ff57c6fa359f2511da34115
Bruno Bulgaron <brunohbulgaron@gmail.com>
2024-09-14 12:01:59 -0300

    script para gerar json referente aos remedios

---
+++ guarulhos.py
@@ -0,0 +1,225 @@
+ubs_guarulhos = [
+    {
+        "nome": "UBS - Jardim Paraventi",
+        "endereco": "R. Villa Lobos, 340"
+    },
+    {
+        "nome": "UBS - Vila Galvão",
+        "endereco": "Av. Doná Eugênia Machado da Silva, 354"
+    },
+    {
+        "nome": "UBS - Vila Barros",
+        "endereco": "R. Carlos Korkisho, 300"
+    },
+    {
+        "nome": "UBS - Tranquilidade",
+        "endereco": "Av. Emílio Ribas, 1897"
+    }
+]
+
+remedios_guarulhos = [
+    {
+        "nome": 'ACICLOVIR 200MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'AMOXICILINA 500MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'AZITROMICINA 500MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'BECLOMETASONA 200 MCG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'BIPERIDENO 2MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'BUDESONIDA 32 MCG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'CLONAZEPAM 0,5MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'CLOPIDOGREL 75MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'CLORPROMAZINA 100MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'DIAZEPAM 5MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'DIGOXINA 0,25MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'DIPIRONA SÓDICA 500MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'ENALAPRIL 10MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'ESTRADIOL 1MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'ESTRIOL 1MG/G',
+        "endereco": '' 
+    },
+    {
+        "nome": 'FENITOÍNA 100MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'FENOBARBITAL 100MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'FLUOXETINA 20MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'GLIBENCLAMIDA 5MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'GLICLAZIDA 60MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'GLYCINE MAX 150MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'HALOPERIDOL 5MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'HIDRALAZINA 25MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'HIDROCLOROTIAZIDA 25MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'IBUPROFENO 600MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'IMIPRAMINA 25MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'INSULINA HUMANA REGULAR 100 UI/ML - 10 ML',
+        "endereco": '' 
+    },
+    {
+        "nome": 'LEVOFLOXACINO 500MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'LEVOMEPROMAZINA 100MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'LEVOMEPROMAZINA 25MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'MEDROXIPROGESTERONA 10MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'METFORMINA 850MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'METILDOPA 250MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'NALTREXONA 50MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'NISTATINA 100.000 UI/ML - 50 ML',
+        "endereco": '' 
+    },
+    {
+        "nome": 'NITROFURANTOÍNA 100MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'ÓLEO MINERAL 100% - 100 ML',
+        "endereco": '' 
+    },
+    {
+        "nome": 'OMEPRAZOL 20MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'PARACETAMOL 500MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'PENTOXIFILINA 400MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'PROPRANOLOL 40MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'RISPERIDONA 1MG/ML - 30 ML',
+        "endereco": '' 
+    },
+    {
+        "nome": 'RIVAROXABANA 20MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'SALBUTAMOL 100 MCG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'SECNIDAZOL 1 G',
+        "endereco": '' 
+    },
+    {
+        "nome": 'SERTRALINA 50MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'TIAMINA (VITAMINA B1) 300MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'TOBRAMICINA 0,3% - 5 ML',
+        "endereco": '' 
+    },
+    {
+        "nome": 'TRAMADOL 50MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'VARFARINA SODICA 5MG',
+        "endereco": '' 
+    },
+    {
+        "nome": 'VENLAFAXINA 75MG',
+        "endereco": '' 
+    }
+]
